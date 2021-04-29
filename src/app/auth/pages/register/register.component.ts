3bcac03750c3d5ea1e9f3d3a104c0d97840d7785
Felipe Larraguibel <felipelarra.araya@gmail.com>
2021-04-28 22:05:25 -0300

    First commit Implementación de login - rutas -lazy load y diseño

---
+++ src/app/auth/pages/register/register.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styles: [
+  ]
+})
+export class RegisterComponent  {
+
+
+  miFormulario: FormGroup = this.fb.group({
+    name:['',[Validators.required, Validators.minLength(6)] ],
+    email:['',[Validators.required, Validators.email] ],
+    password:['',[Validators.required, Validators.minLength(6)] ]
+  });
+
+  constructor( private fb: FormBuilder) { }
+
+  registrar(){
+    console.log(this.miFormulario.valid);
+    console.log(this.miFormulario.value);
+  }
+  
+}
