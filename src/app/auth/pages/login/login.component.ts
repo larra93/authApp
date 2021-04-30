65919be4e37641adb2c3c3e89cfe91c5110c54bf
Felipe Larraguibel <felipelarra.araya@gmail.com>
2021-04-29 22:09:22 -0300

    Implementación de servicio para validar token

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -14,11 +17,26 @@ export class LoginComponent  {
     password: ['123456',[Validators.required, Validators.minLength(6) ]],
   });
 
-  constructor( private fb: FormBuilder ) { }
+  constructor( private fb: FormBuilder,
+               private router: Router,
+               private authSerevice: AuthService ) { }
 
   login(){
+    
     console.log(this.miFormulario.value);
-    console.log(this.miFormulario.valid);
+    const { email, password } = this.miFormulario.value;
+    
+    this.authSerevice.login( email, password )
+        .subscribe( resp => {
+          console.log( resp );
+         if ( resp === true ){
+          this.router.navigateByUrl('/dashboard');
+         }else{
+           Swal.fire('Error', resp, 'error');
+         } 
+        })
+
+    
   }
 
 }
